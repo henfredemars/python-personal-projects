f4112b59c1b6e77515481d487bb20380cd9064ac
henfredemars <henfredemars@users.noreply.github.com>
2015-07-26 23:01:11 -0500

    Create Config.py

---
+++ PyArchive/Config.py
@@ -0,0 +1,9 @@
+# Global PyArchive configuration
+
+email = "example@fastmail.com"
+password = "password"
+server = "mail.messagingengine.com"
+folders = ["INBOX.Archive",'"INBOX.Sent Items"']
+database = "/home/user/PyArchive/mail.db"
+log = "/home/user/PyArchive/log"
+port = 993
