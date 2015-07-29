ac242c3e93859e363d847c1fbba9acb310a22c0f
henfredemars <henfredemars@users.noreply.github.com>
2015-07-28 23:46:11 -0500

    Update Search.py

--- PyArchive/Search.py
+++ PyArchive/Search.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-# Tool to help searching the database of emails for a regex
+# Tool to help searching the database
 
-import argparse 
+import argparse
 import sqlite3
 import re
 
@@ -16,7 +16,12 @@ src = input('List of allowed src: ').split()
 forbidden_src = input('List of forbidden src: ').split()
 dst = input('List of allowed dst: ').split()
 forbidden_dst = input('List of forbidden dst: ').split()
-regex = re.compile(input('Email regex: '))
+regexs = []
+try:
+  while True:
+    regexs.append(re.compile(input('Email regex {} [CTRL-D]: '.format(len(regexs)))))
+except EOFError:
+  print()
 
 #src
 query = 'select raw from email where ('
@@ -30,7 +35,7 @@ for item in forbidden_src:
 if len(forbidden_src):
   query = query[0:-5]
   query += ") and ("
-
+  
 #dst
 for item in dst:
   query += "dst='"+item+"' or "
@@ -42,7 +47,7 @@ for item in forbidden_dst:
 if len(forbidden_dst):
   query = query[0:-5]
 
-#trim, ugly but it works in constant time
+#trim
 if query[-1]=='(':
   query = query[:-2]
   query += ';'
@@ -59,12 +64,19 @@ if query[-6:-1]=='where':
   query += ';'
 
 print(query)
+reqMatchCount = len(regexs)
 for row in conn.execute(query):
   raw = row[0]
-  if regex.search(raw):
-    if len(raw)>2001:
-      print(raw[0:2000])
-    else:
-      print(raw)
+  matchCount = 0
+  for regex in regexs:
+    if regex.search(raw):
+      matchCount += 1
+      if matchCount==reqMatchCount:
+        if len(raw)>14001:
+          print(raw[0:14000])
+        else:
+          print(raw)
+
 
 conn.close()
+
