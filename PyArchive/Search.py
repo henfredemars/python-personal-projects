c91e0c8594322e58a4a0580447d78d6877dd1e2d
henfredemars <henfredemars@users.noreply.github.com>
2015-08-06 18:18:58 -0500

    Added support for fast full text searching

--- PyArchive/Search.py
+++ PyArchive/Search.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 # Tool to help searching the database
 
-import argparse
+import argparse 
 import sqlite3
-import re
 
 # Arguments
 parser = argparse.ArgumentParser(description='Interactive SQLite3 Search Tool')
@@ -16,15 +15,10 @@ src = input('List of allowed src: ').split()
 forbidden_src = input('List of forbidden src: ').split()
 dst = input('List of allowed dst: ').split()
 forbidden_dst = input('List of forbidden dst: ').split()
-regexs = []
-try:
-  while True:
-    regexs.append(re.compile(input('Email regex {} [CTRL-D]: '.format(len(regexs)))))
-except EOFError:
-  print()
+search_term = input('Search string: ')
 
 #src
-query = 'select raw from email where ('
+query = 'select raw from email inner join email_text on email.mid=email_text.mid where ('
 for item in src:
   query += "src='"+item+"' or "
 if len(src):
@@ -35,7 +29,7 @@ for item in forbidden_src:
 if len(forbidden_src):
   query = query[0:-5]
   query += ") and ("
-  
+
 #dst
 for item in dst:
   query += "dst='"+item+"' or "
@@ -46,37 +40,13 @@ for item in forbidden_dst:
   query += "not dst='"+item+"' and "
 if len(forbidden_dst):
   query = query[0:-5]
+  query += ") and ("
 
-#trim
-if query[-1]=='(':
-  query = query[:-2]
-  query += ';'
-else:
-  query += ');'
-if query[-3:]=='and':
-  query = query[:-4]
-  query += ';'
-if query[-4:-1]=='and':
-  query = query[0:-4]
-  query += ';'
-if query[-6:-1]=='where':
-  query = query[0:-6]
-  query += ';'
+#match
+query += "raw match '" + search_term + "')"
 
 print(query)
-reqMatchCount = len(regexs)
-for row in conn.execute(query):
-  raw = row[0]
-  matchCount = 0
-  for regex in regexs:
-    if regex.search(raw):
-      matchCount += 1
-      if matchCount==reqMatchCount:
-        if len(raw)>14001:
-          print(raw[0:14000])
-        else:
-          print(raw)
-
+for item in conn.execute(query):
+  print(item)
 
 conn.close()
-
